b20471cd523b578d511e2945d995613cb8658405
StevenCalvo <scalvom@student.invenio.ac.cr>
2022-07-29 15:26:56 -0600

    30 minutos

--- laborio1stevencalvomurillo.py
+++ laborio1stevencalvomurillo.py
@@ -3,6 +3,9 @@ Nombres=["Lindi","Abram","Salomi","Estel","Kathlin","Adina","Raeann","Bentlee","
 apellidos=["Leithgoe","Dobney","Pinckstone","Fehners","Deary","Crumbleholme","Kovacs","Davion","Veronique","Doerffer","Vowells","Baughn","Carletto","Copland","Bringloe","Armell","Deppe","Jacobsohn","Maggorini","Shier","Morilla","Davsley","Lawless","Arrigucci","Battersby","Karlolczak","Robken","Nairns","Kubyszek","Wagenen","Glasbey","Colisbe"]
 departamento=["Operativos","Administrativos","Socios","Operativos","Administrativos","Operativos","Administrativos","Socios","Administrativos","Operativos","Administrativos","Socios","Administrativos","Operativos","Socios","Administrativos","Operativos","Socios","Administrativos","Operativos","Socios","Operativos","Administrativos","Socios","Operativos","Administrativos","Operativos","Administrativos","Socios","Administrativos","Operativos","Administrativos"]
 salarios=[551690,431298,795406,890987,1160645,1000154,652182,1131439,391010,1113810,437702,771209,938783,410205,664115,440158,1066497,645260,1135481,419851,1128536,530349,837626,529910,512743,1132685,1085963,798176,742414,895712,872395,1125305]
-
+print("Empresa chapulin")
+aumento1=0.35
+aumento2=2.30
 for a,b,c,d,e in zip(id,Nombres,apellidos,departamento,salarios):
+
     
